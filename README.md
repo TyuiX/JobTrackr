179155b9f1cdfa08e784d488d3d0fe19908e72f3
Jason Zhang <jason.zhang.1@stonybrook.edu>
2023-06-17 21:57:47 -0400

    setup

--- README.md
+++ README.md
@@ -19,14 +19,13 @@ Valid status inputs:
 Options:
 - `-m`, `--make`: creating file to store new filelist with name typed in the argument.
 - `-a`, `--add [other flag]`: add job application to the joblist with default status of applied. 
-- `-u`, `--update [other flag]`: update an status of an job application
+- `-u`, `--update [other flag]`: update an status of an job application and update to current date if it turn to interview, offered, unless specify otherwise
 - `-n`, `--name`: name of the company, required to be enter after add or update flag, optional for after lookUp flag
 - `-p`, `--position`: position applied for, required to be enter after add or update flag, optional for after lookup flag
 - `-s`, `--status`: current status optional to be enter after add flag, optional for after lookup flag
-- `-d`, `--date`: date applied optional to be enter after add, optional for after lookup flag
+- `-d`, `--date`: date applied optional to be enter after add and update, optional for after lookup flag. The format of input of the date must be in mxxdxxyxxxx.
 - `-i`, `--id`: the job id number displayed in the list, required to be inputed after the update flag
-- `-l`, `--list`: list the all application
-- `-lu`, `--lookUp [other flag]`: query specific parameter, if no parameter is inputted, it will default to listing all application.
+- `-l`, `--list [optional flag]`: list the all application,  query specific parameter, if no parameter is inputted, it will default to listing all application.
 - `-b`, `--before`: jobs posted before this date, must be optionally inputted after lookUp flag
 - `-af`, `--after`: jobs posted after this date, must be optionally inputted after lookUp flag
-- `-g`, `--ghosted [x days]`: list out companies that never responded in x certain days, default is to set as 21 days.
+- `-g`, `--ghosted [x days]`: list out companies that never responded in x certain days since last update, default is to set as 21 days.
--- jobTrackr.py
+++ jobTrackr.py
@@ -1,2 +1,142 @@
 import getopt
-import sys
+import sys, os, datetime
+arguments = sys.argv
+filename = None
+
+# Define the flags as powers of 2 for bitmasking 
+FLAG_A = 1  
+FLAG_U = 2  
+FLAG_L = 4
+FLAG_B = 8
+FLAG_AF = 16
+FLAG_G = 32
+
+
+# Initialize the flags
+flags = 0
+
+#parse and handle the arguments
+
+def parser(argv):
+    short_options = 'm:a:u:n:p:s:d:i:l:b:af:g:'
+    long_options = ['make=', 'add=', 'update=', 'name=', 'position=', 'status=',
+                    'date=', 'id=', 'list', 'before=', 'after=', 'ghosted=']
+    
+    #initalize stored value
+    argv = sys.argv
+    if (argv.length == 1):
+        print('No arguments was given')
+        sys.exit(2)
+    name = None
+    position = None
+    status = 'applied'
+    id = None
+    ghosted = 21
+    current_datetime = datetime.now()
+    current_year = current_datetime.strftime("%Y")  # 4-digit year
+    current_month = current_datetime.strftime("%B")  # Full month name
+    current_day = current_datetime.strftime("%d")
+    file = sys.argv[-1]
+
+    try:
+        # Parse the command-line arguments
+        opts, args = getopt.getopt(argv[1:-1], short_options, long_options)
+    except getopt.GetoptError:
+        # Handle invalid arguments or options
+        print('Invalid arguments: usage python jobTrackr.py [options] filename')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-m', '--make'):
+            make(argv[-1])
+        elif opt in ('-a', '--add'):
+            flags += FLAG_A
+        elif opt in ('-u', '--update'):
+            flags += FLAG_U
+        elif opt in ('-n', '--name'):
+            if not arg:
+                raise ValueError
+            name = arg
+            pass
+        elif opt in ('-p', '--position'):
+            if not arg:
+                raise ValueError
+            position = arg
+            pass
+        elif opt in ('-s', '--status'):
+            if not arg:
+                raise ValueError
+            position = arg
+            pass
+        elif opt in ('-d', '--date'):
+            if not arg:
+                raise ValueError
+            #pare data return an array 
+            date = parseDate(arg)
+            current_year = date[0]  
+            current_month =  date[1]
+            current_day =  date[2]
+            if not arg:
+                raise ValueError
+            id = arg
+        elif opt in ('-l', '--list'):
+            flags += FLAG_L
+        elif opt in ('-b', '--before'):
+            flags += FLAG_B
+        elif opt in ('-af', '--after'):
+            flags += FLAG_AF
+        elif opt in ('-g', '--ghosted'):
+            if arg:
+                ghosted = arg
+            flags += FLAG_G
+        else:
+            print('Unrecognized option', opt)
+            raise SyntaxError
+    
+    #process
+
+    if flags & FLAG_A:
+        #see if there other flag that dosent belong with add flag
+        if flags & 62:
+            raise SyntaxError
+        add(name, position, status,  current_year, current_month, current_day)
+    elif flags & FLAG_U:
+        if flags & 61:
+            raise SyntaxError
+        update(id, name, position, status,  current_year, current_month, current_day)
+    elif flags & FLAG_L:
+        if flags & 61:
+            raise SyntaxError
+        update(id, name, position, status,  current_year, current_month, current_day)
+
+
+
+
+#create new file
+def make(filename):
+    if not os.path.exists(filename):
+        try:
+            # Open the file in write mode
+            file = open(filename, 'w')
+            file.close()
+            print("File created successfully!")
+
+        except IOError:
+            print("An error occurred while creating the file.")
+    else:
+        print("File already exists. No action taken.")
+
+def openFile(filename):
+    pass
+
+def add(name, position, status,  current_year, current_month, current_day):
+    pass
+
+def update(args):
+    pass
+
+def list(options):
+    pass
+
+#return array as year month day
+def parseDate(date):
+    pass
